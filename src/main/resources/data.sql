a85409ac16b5d1b6dc38d22f4944f00a4c1e1ac6
Budevich <budevichac01@gmail.com>
2023-09-04 16:21:23 +0300

    refactored

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO client (name, sername)
+INSERT INTO client (name, surname)
 VALUES ('Андрей', 'Будевич'),
        ('Татьяна', 'Будевич'),
        ('Ксения', 'Будевич');
 
 INSERT INTO contact (client_id, contact_type, contact_info)
-VALUES (1, EMAIL, 'budevichac@gmail.com'),
-       (1, PHONE_NUMBER, '+375297634349'),
-       (2, PHONE_NUMBER, '+375447324144'),
-       (3, EMAIL, 'budevichka@gmail.com');
+VALUES (1, 'EMAIL', 'budevichac@gmail.com'),
+       (1, 'PHONE_NUMBER', '+375297634349'),
+       (2, 'PHONE_NUMBER', '+375447324144'),
+       (3, 'EMAIL', 'budevichka@gmail.com');
