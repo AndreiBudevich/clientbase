90027bef52bfb8f02d856ea07ccc5ac73a01501e
Budevich <budevichac01@gmail.com>
2023-09-04 16:55:23 +0300

    create HasId

---
+++ src/main/java/com/client/clientbase/HasId.java
@@ -0,0 +1,21 @@
+package com.client.clientbase;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.util.Assert;
+
+public interface HasId {
+
+    Long getId();
+
+    void setId(Long id);
+
+    @JsonIgnore
+    default boolean isNew() {
+        return getId() == null;
+    }
+
+    default long id() {
+        Assert.notNull(getId(), "Entity must has id");
+        return getId();
+    }
+}
