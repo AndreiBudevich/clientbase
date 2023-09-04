40787c1c479475eec09bb18e5d6705329c831e07
Budevich <budevichac01@gmail.com>
2023-09-04 16:14:37 +0300

    create populate data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO client (name, sername)
+VALUES ('Андрей', 'Будевич'),
+       ('Татьяна', 'Будевич'),
+       ('Ксения', 'Будевич');
+
+INSERT INTO contact (client_id, contact_type, contact_info)
+VALUES (1, EMAIL, 'budevichac@gmail.com'),
+       (1, PHONE_NUMBER, '+375297634349'),
+       (2, PHONE_NUMBER, '+375447324144'),
+       (3, EMAIL, 'budevichka@gmail.com');
