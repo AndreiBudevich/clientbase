61412ac342cdda60279f965f71ab0c1d47c9d839
Budevich <budevichac01@gmail.com>
2023-09-04 15:36:15 +0300

    create Client

---
+++ src/main/java/com/client/clientbase/model/Client.java
@@ -0,0 +1,35 @@
+package com.client.clientbase.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "client")
+class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "client")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonBackReference(value = "client-contact")
+    private List<Contact> contacts;
+}
